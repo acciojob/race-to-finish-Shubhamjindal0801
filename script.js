dc19af1b2509c15390580b6d9585840efa4be111
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-10 21:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.promises = [];
-
-// Do not change the code above this
-// add your promises to the array `promises`
+window.promises = [
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 1")
+        },1000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 2")
+        },2000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 3")
+        },3000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 4")
+        },4000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 5")
+        },5000)
+    })
+]
+document.getElementById("output").innerHTML = Promise.any(promises)
